58922b84957ef3a0d2582f338c1ebb65c9cde3a2
tvanantha <anantha83@gmail.com>
2020-10-08 15:24:49 +0530

    build_sl

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,2 +1,9 @@
 #!/usr/bin/env groovy
 
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}
